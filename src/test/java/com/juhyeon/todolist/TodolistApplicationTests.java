5aa3666349fa864cce25fb6576a70eb934f154b6
DESKTOP-NUOPUIF\ITPS <itps@google.com>
2022-07-25 20:10:17 +0900

    ToDoList_Juhyeon 1stCommit

---
+++ src/test/java/com/juhyeon/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhyeon.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
